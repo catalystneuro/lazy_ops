4d39da9a7ca243c44940d53725598dc9d8eec867
d-sot <54951034+d-sot@users.noreply.github.com>
2019-09-17 09:13:42 -0400

    Update lazy_loading.py

--- lazy_loading.py
+++ lazy_loading.py
@@ -9,7 +9,7 @@ from lazy_loading import DataSetViews
 dsetviews = DataSetViews(dataset) # dataset is an instantiated h5py dataset
 view1 = dsetviews.lazy_slice[1:10:2,:,0:50:5].lazy_transpose([2,0,1]).lazy_slice[25:55,1,1:4:1,:].transpose()
 
-A = view1.dsetread  # stores view1 of h5py dataset data in the A array
+A = view1.dsetread  # reads view1 of h5py dataset data
 B = dsetviews[:] # Brackets on DataSetView without lazy_slice call the h5py slicing method, that returns dataset data
 """
 
