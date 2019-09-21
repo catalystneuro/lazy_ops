808086d8d5cce5ee70098df13d904b76845151c8
dsot <dsot@protonmail.com>
2019-09-21 17:33:28 -0400

    Updates to DatasetView class: merged with LazySlice class                               Fixed brackets on lazy slices to read data                               added LazyShape Updates to test             : minor changes

--- lazy_loading.py
+++ lazy_loading.py
@@ -1,106 +1,118 @@
 """Provides a class to allow for lazy transposing and slicing operations on h5py datasets
-
 Example Usage:
-
 import h5py
 from lazy_loading import DataSetView
 .
 .
 dsetview = DatasetView(dataset) # dataset is an instantiated h5py dataset
-view1 = dsetview.lazy_slice[1:10:2,:,0:50:5].lazy_transpose([2,0,1]).lazy_slice[25:55,1,1:4:1,:].transpose()
+view1 = dsetview.LazySlice[1:10:2,:,0:50:5].LazyTranspose([2,0,1]).LazySlice[25:55,1,1:4:1,:]
+A = view1[:]          # Brackets on DataSetView call the h5py slicing method, that returns dataset data
+B = view1.dsetread()  # same as view1[:]
 
-A = view1.dsetread  # reads view1 of h5py dataset data
-B = dsetview[:] # Brackets on DataSetView without lazy_slice call the h5py slicing method, that returns dataset data
 """
 
 import h5py
 import numpy as np
 
-
 class DatasetView(h5py.Dataset):
-    """ Inherit from this class when implementing dataset views """
 
-    def __init__(self, dataset: h5py.Dataset):
+    def __init__(self, dataset : h5py.Dataset = None, slice_index=np.index_exp[:], axis_order=None):
         """
         Args:
-          dataset: Underlying HDF5 dataset returned from h5py
-        Returns:
-          Dataset view object
-        """
-        self.__slice_key = np.index_exp[:]
-        h5py.Dataset.__init__(self, dataset.id)
-        self.lazy_slice = LazySlice(self)
-
-    def dsetread(self):
-        """ Returns the data
-        Returns:
-          numpy array
-        """
-        return self[:]
-
-    def lazy_transpose(self, axis_order=None):
-        """ Array lazy transposition
-        Args:
-          axis_order: permutation order for transpose
-        Returns:
-          dataset view object
-        """
-        return self.lazy_slice.lazy_transpose(axis_order)
-
-    def transpose(self, axis_order=None):
-        """ Same as lazy_transpose() """
-        return self.lazy_transpose(axis_order)
-
-    def T(self, axis_order=None):
-        """ Same as lazy_transpose() """
-        return self.lazy_transpose(axis_order)
-
-
-class LazySlice(object):
-    def __init__(self, dview, key=np.index_exp[:], axis_order=None):
-        """
-        Args:
-          dview:      the underlying view object referring to the dataset
-          key:        the aggregate slice after multiple lazy slicing
+          dataset:    the underlying dataset
+          slice_index:  the aggregate slice after multiple lazy slicing
           axis_order: the aggregate axis_order after multiple transpositions
         Returns:
           lazy object of the view
         """
+
+        h5py.Dataset.__init__(self, dataset.id)
         if axis_order is None:
-            self._axis_order = list(range(len(dview.shape)))
+            self._axis_order = list(range(len(dataset.shape)))
         else:
             self._axis_order = axis_order
-        self._key = key
-        self._dview = dview
-        self.lazy_slice = self
+        self._lazy_slice_call = False
+        self._dataset = dataset
+        self._LazyShape, self._key = self._slice_shape(slice_index)
+        self._LazySlice = self
 
     @property
-    def dview(self):
-        return self._dview
+    def LazySlice(self):
+        ''' Indicator for LazySlice calls '''
+        self._lazy_slice_call = True
+        return self._LazySlice
+
+    @property
+    def dataset(self):
+        return self._dataset
+
+    @property
+    def LazyShape(self):
+        """ The shape due to the lazy operations  """
+        return self._LazyShape
 
     @property
     def key(self):
-        """ the self.key slice is passed to the lazy instance and is not altered the instance's init call """
+        """ The self.key slice is passed to the lazy instance and is not altered by the instance's init call """
         return self._key
 
     @property
     def axis_order(self):
         return self._axis_order
 
-    def __getitem__(self, new_slice):
-        """  supports python's colon slicing syntax """
+    def _slice_tuple(self, key):
+        """  Allows single slice function calls
+        Args:
+          key: The slice object
+        Returns:
+          The slice object tuple
         """
+        if isinstance(key, slice):
+            key = key,
+        else:
+            key = *key,
+
+        return key
+
+    def _slice_shape(self,slice_):
+        """  For an slice returned by slice_composition function, finds the shape
+        Args:
+          slice_: The slice object
+        Returns:
+          slice_shape: Shape of the slice object
+          slice_key: An equivalent slice tuple with positive starts and stops
+        """
+        slice_ = self._slice_tuple(slice_)
+        # converting the slice to regular slices that only contain integers
+        slice_regindices = [slice(*slice_[i].indices(self.dataset.shape[self.axis_order[i]])) for i in range(len(slice_))]
+        slice_shape = ()
+        for i in range(len(slice_)):
+            slice_start, slice_stop, slice_step = slice_regindices[i].start, slice_regindices[i].stop, slice_regindices[i].step
+            assert slice_start >= 0 and slice_stop >= 0 and slice_step >= 1
+            if slice_step < 1:
+                raise ValueError("Slice step parameter must be positive")
+            if slice_stop < slice_start:
+                slice_start = slice_stop
+                slice_regindices[i] = slice(slice_start, slice_stop, slice_step)
+            slice_shape += (1 + (slice_stop - slice_start -1 )//slice_step if slice_stop != slice_start else 0,)
+
+        slice_regindices = tuple(slice_regindices)
+        return slice_shape, slice_regindices
+
+    def __getitem__(self, new_slice):
+        """  supports python's colon slicing syntax 
         Args:
           new_slice:  the new slice to compose with the lazy instance's self.key slice
         Returns:
           lazy object of the view
         """
-        if isinstance(new_slice, slice):
-            new_slice = new_slice,
-        else:
-            new_slice = *new_slice,
-        self.key_reinit = self.slice_composition(new_slice)
-        return LazySlice(self.dview, self.key_reinit, self.axis_order)
+        if self._lazy_slice_call:
+            self._lazy_slice_call = False
+            new_slice = self._slice_tuple(new_slice)
+            self.key_reinit = self.slice_composition(new_slice)
+            return DatasetView(self.dataset, self.key_reinit, self.axis_order)
+
+        return self.dsetread()[new_slice]
 
     def __call__(self, new_slice):
         """  allows LazySlice function calls with slice objects as input"""
@@ -113,9 +125,9 @@ class LazySlice(object):
         """
         # Note: Directly calling regionref with slices with a zero dimension does not
         # retain shape information of the other dimensions
-        self.reversed_axis_order = sorted(range(len(self.key)), key=lambda i: self.axis_order[i])
-        reversed_slice_key = tuple(self.key[i] for i in self.reversed_axis_order)
-        return self.dview[reversed_slice_key].transpose(self.axis_order)
+        self.reversed_axis_order = sorted(range(len(self.axis_order)), key=lambda i: self.axis_order[i])
+        reversed_slice_key = tuple(self.key[i] for i in self.reversed_axis_order if i < len(self.key))
+        return self.dataset[reversed_slice_key].transpose(self.axis_order)
 
     def slice_composition(self, new_slice):
         """  composes a new_slice with the self.key slice
@@ -124,45 +136,51 @@ class LazySlice(object):
         Returns:
           merged slice object
         """
-        slice_list = []
-        # Iterating over the new slicing tuple to change the merge dataset slice.
+        slice_result = ()
+        # Iterating over the new slicing tuple to change the merged dataset slice.
         for i in range(len(new_slice)):
             if i < len(self.key):
-                # converting last stored key slice to regular slices that only contain integers
-                pre_start, pre_stop, pre_step = self.key[i].indices(self.dview.shape[self.axis_order[i]])
-                assert pre_start >= 0 and pre_stop >= 0 and pre_step >= 1
-                if pre_stop < pre_start:
-                    pre_start = pre_stop
-                pre_shape = 1 + (pre_stop - pre_start -1 )//abs(pre_step) if pre_stop != pre_start else 0  # array dimension after last slice
+                # converting new_slice slice to regular slices,
+                # newkey_start, newkey_stop, newkey_step only contains positive or zero integers
+                newkey_start, newkey_stop, newkey_step = new_slice[i].indices(self.LazyShape[i])
+                if newkey_step < 1:
+                    # regionref requires step>=1 for dataset data calls
+                    raise ValueError("Slice step parameter must be positive")
+                if newkey_stop < newkey_start:
+                    newkey_start = newkey_stop
+                assert newkey_start >= 0 and newkey_stop >= 0
 
-                # converting new_slice slice to regular slices that only contain integers
-                newkey_directslice = new_slice[i].indices(pre_shape)
-                newkey_start, newkey_stop, newkey_step = newkey_directslice
-                # newkey_directslice only contains positive or zero integers
-                # regionref requires step>=1 for dataset data calls
-                assert newkey_start >= 0 and newkey_stop >= 0 and newkey_step >=1
-                slice_list.append(slice(pre_start + pre_step * newkey_start, min(pre_start + pre_step * newkey_stop , pre_stop), newkey_step * pre_step))
+                slice_result += (slice(min(self.key[i].start + self.key[i].step * newkey_start, self.key[i].stop), 
+                                 min(self.key[i].start + self.key[i].step * newkey_stop , self.key[i].stop),
+                                 newkey_step * self.key[i].step),)
+                assert (self.key[i].start + self.key[i].step * newkey_start) >=0 and min(self.key[i].start + self.key[i].step * newkey_stop , self.key[i].stop) >=0 and (newkey_step * self.key[i].step) >=1
+                assert min(self.key[i].start + self.key[i].step * newkey_stop , self.key[i].stop) >= min(self.key[i].start + self.key[i].step * newkey_start, self.key[i].stop)
             else:
-                slice_list.append(slice(*new_slice[i].indices(self.dview.shape[self.axis_order[i]])))
+                slice_result += (slice(*new_slice[i].indices(self.dataset.shape[self.axis_order[i]])),)
         for i in range(len(new_slice), len(self.key)):
-            slice_list.append(slice(*self.key[i].indices(self.dview.shape[self.axis_order[i]])))
-        slice_result = tuple(slice_list)
+            slice_result += (slice(*self.key[i].indices(self.dataset.shape[self.axis_order[i]])),)
+
         return slice_result
 
     def transpose(self, axis_order=None):
-        """ Same as lazy_transpose() """
-        return self.lazy_transpose(axis_order)
+        """ Returns the transposed data
+        Args:
+          axis_order: permutation order for data transpose
+        Returns:
+          numpy array
+        """
+        return self.dsetread().transpose(axis_order)
 
     def T(self, axis_order=None):
-        """ Same as lazy_transpose() """
-        return self.lazy_transpose(axis_order)
+        """ Same as transpose() """
+        return self.transpose(axis_order)
 
-    def lazy_transpose(self, axis_order=None):
+    def LazyTranspose(self, axis_order=None):
         """ Array lazy transposition, no axis_order reverses the order of dimensions
         Args:
           axis_order: permutation order for transpose
         Returns:
-          lazy ojbect of the view
+          lazy object of the view
         """
 
         if axis_order is None:
@@ -170,7 +188,6 @@ class LazySlice(object):
 
         self.key_reinit = [self.key[i] if i < len(self.key) else np.s_[:] for i in axis_order]
 
-        axis_order_reinit = [self.axis_order[i] for i in axis_order]
-        return LazySlice(self.dview, self.key_reinit, axis_order_reinit)
-
+        axis_order_reinit = [self.axis_order[i] if i < len(self.axis_order) else i for i in axis_order]
+        return DatasetView(self.dataset, self.key_reinit, axis_order_reinit)
 
--- test_file.py
+++ test_file.py
@@ -6,20 +6,21 @@ import secrets
 from random import shuffle
 from numpy.testing import assert_array_equal
 
-
 def main():
 
     f = h5py.File('testfile.hdf5')
-    dset = f.create_dataset(name='test_dataset', data=np.random.rand(10, 100000, 30))
+    ndims = 4
+    secret_rand = secrets.SystemRandom()
+    dset = f.create_dataset(name='test_dataset', data=np.random.rand(*secret_rand.sample(range(1, 200//ndims), ndims)))
     dsetview = DatasetView(dset)
 
-    secret_rand = secrets.SystemRandom()
     randslice = lambda d: slice(*secret_rand.sample(range(-dsetview.shape[d] * 5 // 4, dsetview.shape[d] * 5 // 4), 1),
                                 *secret_rand.sample(range(-dsetview.shape[d] * 5 // 4, dsetview.shape[d] * 5 // 4), 1),
-                                *secret_rand.sample(range(1, dsetview.shape[d]), 1))
+                                *secret_rand.sample(range(1, dsetview.shape[d] + 1), 1))
 
     for _ in range(200):
-        slice_list = [np.index_exp[:],  # randslice(0),np.s_[:],np.s_[:]
+        slice_list = [randslice(0),
+                      np.s_[:],
                       np.index_exp[:],
                       tuple(randslice(i) for i in range(len(dsetview.shape))),
                       tuple(randslice(i) for i in range(len(dsetview.shape))),
@@ -27,77 +28,55 @@ def main():
                       tuple(randslice(i) for i in range(len(dsetview.shape))),
                       tuple(randslice(i) for i in range(len(dsetview.shape)))]
 
-        slice_0 = secrets.choice(slice_list)
-        slice_1 = secrets.choice(slice_list)
-        slice_2 = secrets.choice(slice_list)
-        slice_3 = secrets.choice(slice_list)
-        slice_4 = secrets.choice(slice_list)
+        slice_list = [secrets.choice(slice_list) for i in range(5)]
 
-        shuffle_a = [0, 1, 2]
-        shuffle_b = [0, 1, 2]
-        shuffle_c = [0, 1, 2]
-        shuffle_d = [0, 1, 2]
-        shuffle_e = [0, 1, 2]
+        shuffle_list = [list(range(ndims)) for i in range(5)]
+        for li in shuffle_list: shuffle(li)
 
-        shuffle(shuffle_a)
-        shuffle(shuffle_b)
-        shuffle(shuffle_c)
-        shuffle(shuffle_d)
-        shuffle(shuffle_e)
+        assert_array_equal(dset[slice_list[1]],  dsetview.LazySlice[slice_list[1]][:])
 
-        print("S_0", slice_0)
-        print("S_1", slice_1)
-        print("SSSSz_2", slice_2)
-        print("SSSS_3", slice_3)
-        print("SSSS_3", slice_4)
+        assert_array_equal(dsetview[slice_list[1]][slice_list[2]],  
+              dsetview.LazySlice[slice_list[1]].LazySlice[slice_list[2]][:])
 
-        print("shuffle_a", shuffle_a)
-        print("shuffle_b", shuffle_b)
-        print("shuffle_c", shuffle_c)
-        print("shuffle_d", shuffle_d)
-        print("shuffle_e", shuffle_e)
+        assert_array_equal(dsetview[slice_list[1]][slice_list[2]][slice_list[3]], 
+              dsetview.LazySlice[slice_list[1]].LazySlice[slice_list[2]].LazySlice[slice_list[3]][:])
 
-        assert_array_equal([slice_1][slice_2], dsetview.lazy_slice[slice_1].lazy_slice[slice_2])
+        assert_array_equal(dset[slice_list[1]][slice_list[2]][slice_list[3]],  
+              dsetview.LazySlice[slice_list[1]].LazySlice[slice_list[2]].LazySlice[slice_list[3]][:])
 
-        assert_array_equal(dsetview[slice_1][slice_2][slice_3],
-                           dsetview.lazy_slice[slice_1].lazy_slice[slice_2].lazy_slice[slice_3])
+        assert_array_equal(dset[slice_list[1]].transpose(), dsetview.LazySlice[slice_list[1]].LazyTranspose()[:])
 
-        assert_array_equal(dset[slice_1].transpose(),
-                           dsetview.lazy_slice[slice_1].lazy_transpose())
+        assert_array_equal(dsetview[slice_list[1]].transpose(), dsetview.LazySlice[slice_list[1]].LazyTranspose()[:])
 
-        assert_array_equal(dsetview[slice_1].transpose(),
-                           dsetview.lazy_slice[slice_1].lazy_transpose())
+        assert_array_equal(dsetview[slice_list[1]].transpose()[slice_list[2]], 
+              dsetview.LazySlice[slice_list[1]].LazyTranspose().LazySlice[slice_list[2]][:])
 
-        assert_array_equal(dsetview[slice_1].transpose()[slice_2],
-                           dsetview.lazy_slice[slice_1].lazy_transpose().lazy_slice[slice_2])
+        assert_array_equal(dsetview[slice_list[1]].transpose()[slice_list[2]][slice_list[3]], 
+              dsetview.LazySlice[slice_list[1]].LazyTranspose().LazySlice[slice_list[2]].LazySlice[slice_list[3]][:])
 
-        assert_array_equal(dsetview[slice_1].transpose()[slice_2][slice_3],
-                           dsetview.lazy_slice[slice_1].lazy_transpose().lazy_slice[slice_2].lazy_slice[slice_3])
+        assert_array_equal(dset[slice_list[1]].transpose()[slice_list[2]][slice_list[3]], 
+              dsetview.LazySlice[slice_list[1]].LazyTranspose().LazySlice[slice_list[2]].LazySlice[slice_list[3]][:])
 
-        assert_array_equal(dset[slice_1].transpose()[slice_2][slice_3],
-                           dsetview.lazy_slice[slice_1].lazy_transpose().lazy_slice[slice_2].lazy_slice[slice_3])
+        assert_array_equal( dsetview[slice_list[1]].transpose(shuffle_list[3])[slice_list[2]][slice_list[3]], 
+              dsetview.LazySlice[slice_list[1]].LazyTranspose(shuffle_list[3]).LazySlice[slice_list[2]].LazySlice[slice_list[3]].dsetread())
 
-        assert_array_equal(dset[slice_1].transpose([2, 0, 1]),
-                           dsetview.lazy_slice[slice_1].lazy_transpose([2, 0, 1]))
+        assert_array_equal( dset[slice_list[1]].transpose(shuffle_list[3])[slice_list[2]][slice_list[3]].transpose(shuffle_list[4]),
+             dsetview.LazySlice[slice_list[1]].LazyTranspose(shuffle_list[3]).LazySlice[slice_list[2]]
+             .LazySlice[slice_list[3]].LazyTranspose(shuffle_list[4])[:])
 
-        assert_array_equal(dsetview[slice_1].transpose([2, 0, 1])[slice_2],
-                           dsetview.lazy_slice[slice_1].lazy_transpose([2, 0, 1]).lazy_slice[slice_2])
+        assert_array_equal(dset[:].transpose(shuffle_list[0]), dsetview.LazyTranspose(shuffle_list[0]).dsetread()[:])
 
-        assert_array_equal(dsetview[slice_1].transpose([2, 0, 1])[slice_2][slice_3],
-                           dsetview.lazy_slice[slice_1].lazy_transpose([2, 0, 1]).lazy_slice[slice_2].lazy_slice[slice_3])
+        assert_array_equal(dsetview.transpose(shuffle_list[0])[slice_list[1]].transpose(shuffle_list[1])[slice_list[2]][slice_list[3]]
+             .transpose(shuffle_list[2])[slice_list[4]].transpose(),
+             dsetview.LazyTranspose(shuffle_list[0]).LazySlice[slice_list[1]].LazyTranspose(shuffle_list[1]).
+             LazySlice[slice_list[2]].LazySlice[slice_list[3]].LazyTranspose(shuffle_list[2]).LazySlice[slice_list[4]].LazyTranspose()[:])
 
-        assert_array_equal(dset[slice_1].transpose([2, 0, 1])[slice_2][slice_3].transpose([1, 2, 0]),
-                           dsetview.lazy_slice[slice_1].lazy_transpose([2, 0, 1]).lazy_slice[slice_2].
-                           lazy_slice[slice_3].lazy_transpose([1, 2, 0]))
-
-        assert_array_equal(dset[:, :, :].transpose(shuffle_a)[slice_1].transpose(shuffle_b)[slice_2][slice_3].
-                           transpose(shuffle_c)[slice_4].transpose(),
-                           dsetview.lazy_transpose(shuffle_a).lazy_slice[slice_1].transpose(shuffle_b).
-                           lazy_slice[slice_2].lazy_slice[slice_3].lazy_transpose(shuffle_c).lazy_slice[slice_4].
-                           lazy_transpose())
+        assert_array_equal(dset[:].transpose(shuffle_list[0])[slice_list[1]].transpose(shuffle_list[1])[slice_list[2]][slice_list[3]].
+             transpose(shuffle_list[2])[slice_list[4]].transpose(),
+             dsetview.LazyTranspose(shuffle_list[0]).LazySlice[slice_list[1]].LazyTranspose(shuffle_list[1])
+             .LazySlice[slice_list[2]].LazySlice[slice_list[3]].LazyTranspose(shuffle_list[2]).LazySlice[slice_list[4]].LazyTranspose()[:])
 
     os.remove('testfile.hdf5')
-
 
 if __name__ == "__main__":
     main()
