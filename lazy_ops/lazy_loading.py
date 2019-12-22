85c355959811259a83cf91a6be503ade329f5446
dsot <dsot@protonmail.com>
2019-12-22 17:01:41 -0500

    * Drop dimension with integer slicing

--- lazy_ops/lazy_loading.py
+++ lazy_ops/lazy_loading.py
@@ -17,11 +17,11 @@ import sys
 
 class DatasetView(h5py.Dataset):
 
-    def __init__(self, dataset: h5py.Dataset = None, slice_index=np.index_exp[:], axis_order=None):
+    def __init__(self, dataset: h5py.Dataset = None, slice_index=(np.index_exp[:],()), axis_order=None):
         """
         Args:
           dataset:    the underlying dataset
-          slice_index:  the aggregate slice after multiple lazy slicing
+          slice_index:  the aggregate slice and int indices after multiple lazy calls
           axis_order: the aggregate axis_order after multiple transpositions
         Returns:
           lazy object of the view
@@ -29,12 +29,12 @@ class DatasetView(h5py.Dataset):
 
         h5py.Dataset.__init__(self, dataset.id)
         if axis_order is None:
-            self._axis_order = list(range(len(dataset.shape)))
+            self._axis_order = tuple(range(len(dataset.shape)))
         else:
             self._axis_order = axis_order
         self._lazy_slice_call = False
         self._dataset = dataset
-        self._lazy_shape, self._key = self._slice_shape(slice_index)
+        self._shape, self._key, self._int_index, self._axis_order = self._slice_shape(slice_index)
 
     @property
     def lazy_slice(self):
@@ -48,17 +48,17 @@ class DatasetView(h5py.Dataset):
 
     @property
     def shape(self):
-        return self._lazy_shape
+        return self._shape
 
     def __len__(self):
         return self.len()
 
     def len(self):
-        return self._lazy_shape[0]
+        return self.shape[0]
 
     @property
     def key(self):
-        """ The self.key slice is passed to the lazy instance and is not altered by the instance's init call """
+        """ The self.key slice is passed to the lazy instance """
         return self._key
 
     @property
@@ -87,13 +87,16 @@ class DatasetView(h5py.Dataset):
           slice_shape: Shape of the slice object
           slice_key: An equivalent slice tuple with positive starts and stops
         """
-        slice_ = self._slice_tuple(slice_)
+        int_ind = slice_[1]
+        slice_ = self._slice_tuple(slice_[0])
         # converting the slice to regular slices that only contain integers
-        
         slice_regindices = [slice(*slice_[i].indices(self.dataset.shape[self.axis_order[i]])) if isinstance(slice_[i],slice)
                             else slice_[i]
                             for i in range(len(slice_))]
+        
         slice_shape = ()
+        int_index = ()
+        axis_order = ()
         for i in range(len(slice_)):
             if isinstance(slice_[i],slice):
                 slice_start, slice_stop, slice_step = slice_regindices[i].start, slice_regindices[i].stop, slice_regindices[i].step
@@ -103,12 +106,19 @@ class DatasetView(h5py.Dataset):
                     slice_start = slice_stop
                     slice_regindices[i] = slice(slice_start, slice_stop, slice_step)
                 slice_shape += (1 + (slice_stop - slice_start -1 )//slice_step if slice_stop != slice_start else 0,)
+                axis_order += (self.axis_order[i],)
+            elif isinstance(slice_[i],int):
+                int_index += ((i,slice_[i],self.axis_order[i]),)
             else:
                 # slice_[i] is an iterator of integers
                 slice_shape += (len(slice_[i]),)
-        slice_shape += self.dataset.shape[len(slice_)::]
-        slice_regindices = tuple(slice_regindices)
-        return slice_shape, slice_regindices
+                axis_order += (self.axis_order[i],)
+        slice_regindices = tuple(el for el in slice_regindices if not isinstance(el,int))
+        axis_order += tuple(self.axis_order[len(axis_order)+len(int_index)::])
+        int_index += int_ind
+        slice_shape += self.dataset.shape[len(slice_shape)+len(int_index)::]
+
+        return slice_shape, slice_regindices, int_index, axis_order
 
     def __getitem__(self, new_slice):
         """  supports python's colon slicing syntax 
@@ -120,9 +130,8 @@ class DatasetView(h5py.Dataset):
         key_reinit = self._slice_composition(new_slice)
         if self._lazy_slice_call:
             self._lazy_slice_call = False
-            return DatasetView(self.dataset, key_reinit, self.axis_order)
-
-        return DatasetView(self.dataset, key_reinit, self.axis_order).dsetread()
+            return DatasetView(self.dataset, (key_reinit, self._int_index), self.axis_order)
+        return DatasetView(self.dataset, (key_reinit, self._int_index), self.axis_order).dsetread()
 
     def __call__(self, new_slice):
         """  allows lazy_slice function calls with slice objects as input"""
@@ -135,9 +144,19 @@ class DatasetView(h5py.Dataset):
         """
         # Note: Directly calling regionref with slices with a zero dimension does not
         # retain shape information of the other dimensions
-        reversed_axis_order = sorted(range(len(self.axis_order)), key=lambda i: self.axis_order[i])
-        reversed_slice_key = tuple(self.key[i] for i in reversed_axis_order if i < len(self.key))
-        return self.dataset[reversed_slice_key].transpose(self.axis_order)
+        lazy_axis_order = self.axis_order
+        lazy_key = self.key
+        axis_order_slice_iter = self.axis_order
+        for ind in self._int_index:
+            lazy_axis_order = lazy_axis_order[:ind[0]] + (ind[2],) + lazy_axis_order[ind[0]:]
+            lazy_key = lazy_key[:ind[0]] + (ind[1],) + lazy_key[ind[0]:]
+        for ind in reversed(self._int_index):
+            axis_order_slice_iter = tuple(i if i<ind[2] else i-1 for i in axis_order_slice_iter)
+
+        reversed_axis_order = sorted(range(len(lazy_axis_order)), key=lambda i: lazy_axis_order[i])
+        reversed_slice_key = tuple(lazy_key[i] for i in reversed_axis_order if i < len(lazy_key))
+
+        return self.dataset[reversed_slice_key].transpose(axis_order_slice_iter)
 
     def _slice_composition(self, new_slice):
         """  composes a new_slice with the self.key slice
@@ -155,7 +174,7 @@ class DatasetView(h5py.Dataset):
                 if i < len(self.key):
                     # converting new_slice slice to regular slices,
                     # newkey_start, newkey_stop, newkey_step only contains positive or zero integers
-                    newkey_start, newkey_stop, newkey_step = new_slice[i].indices(self._lazy_shape[i])
+                    newkey_start, newkey_stop, newkey_step = new_slice[i].indices(self._shape[i])
                     if newkey_step < 1:
                         # regionref requires step>=1 for dataset data calls
                         raise ValueError("Slice step parameter must be positive")
@@ -167,32 +186,30 @@ class DatasetView(h5py.Dataset):
                                          newkey_step * self.key[i].step),)
                     else:
                         # self.key[i] is an iterator of integers
-                        slice_result += self.key[i][new_slice[i]]
+                        slice_result += (self.key[i][new_slice[i]],)
                 else:
                     slice_result += (slice(*new_slice[i].indices(self.dataset.shape[self.axis_order[i]])),)
             elif isinstance(new_slice[i],int):
                 if i < len(self.key):
-                    if new_slice[i] >= self._lazy_shape[i] or new_slice[i] <= ~self._lazy_shape[i]:
-                        raise IndexError("Index %d out of range, dim %d of size %d" % (new_slice[i],i,self._lazy_shape[i]))
+                    if new_slice[i] >= self._shape[i] or new_slice[i] <= ~self._shape[i]:
+                        raise IndexError("Index %d out of range, dim %d of size %d" % (new_slice[i],i,self._shape[i]))
                     if isinstance(self.key[i],slice):
-                        int_index = self.key[i].start + self.key[i].step*(new_slice[i]%self._lazy_shape[i])
-                        slice_result += (slice(*slice(int_index,int_index+1 or None, None
-                                                      ).indices(self.dataset.shape[self.axis_order[i]])),)
+                        int_index = self.key[i].start + self.key[i].step*(new_slice[i]%self._shape[i])
+                        slice_result += (int_index,)
                     else:
                         # self.key[i] is an iterator of integers
-                        slice_result += (slice(*slice(self.key[i][new_slice[i]],self.key[i][new_slice[i]]+1 or None, None
-                                                      ).indices(self.dataset.shape[self.axis_order[i]])),)
+                        slice_result += (self.key[i][new_slice[i]],)
                 else:
-                    slice_result += (slice(*slice(new_slice[i],new_slice[i]+1 or None, None).indices(self.dataset.shape[self.axis_order[i]])),)
+                    slice_result += (new_slice[i],)
             else:
                 try:
                     if any(not isinstance(el,int) for el in new_slice[i]):
                         raise ValueError("Indices must be integers")
                     if i < len(self.key):
-                        if any(el >= self._lazy_shape[i] or el <= ~self._lazy_shape[i] for el in new_slice[i]):
-                            raise IndexError("Index %d out of range, dim %d of size %d" % (str(new_slice[i]),i,self._lazy_shape[i]))
+                        if any(el >= self._shape[i] or el <= ~self._shape[i] for el in new_slice[i]):
+                            raise IndexError("Index %s out of range, dim %d of size %d" % (str(new_slice[i]),i,self._shape[i]))
                         if isinstance(self.key[i],slice):
-                            slice_result += (tuple(self.key[i].start + self.key[i].step*(ind%self._lazy_shape[i]) for ind in new_slice[i]),)
+                            slice_result += (tuple(self.key[i].start + self.key[i].step*(ind%self._shape[i]) for ind in new_slice[i]),)
                         else:
                             # self.key[i] is an iterator of integers
                             slice_result += (tuple(self.key[i][ind] for ind in new_slice[i]),)
@@ -200,7 +217,8 @@ class DatasetView(h5py.Dataset):
                         slice_result += (new_slice[i],)
                 except:
                     raise IndexError("Indices must be either integers, iterators of integers, or slice objects")
-        slice_result += self.key[len(new_slice):len(self.key)]
+        slice_result += self.key[len(new_slice):]
+
         return slice_result
 
     @property
@@ -217,14 +235,14 @@ class DatasetView(h5py.Dataset):
         """
 
         if axis_order is None:
-            axis_order = list(reversed(range(len(self.axis_order))))
+            axis_order = tuple(reversed(range(len(self.axis_order))))
 
-        axis_order_reinit = [self.axis_order[i] if i < len(self.axis_order) else i for i in axis_order]
-        key_reinit = [self.key[i] if i < len(self.key) else np.s_[:] for i in axis_order]
-        key_reinit.extend([self.key[i] for i in self.axis_order if i not in axis_order_reinit])
-        axis_order_reinit.extend([i for i in self.axis_order if i not in axis_order_reinit])
+        axis_order_reinit = tuple(self.axis_order[i] if i < len(self.axis_order) else i for i in axis_order)
+        key_reinit = tuple(self.key[i] if i < len(self.key) else np.s_[:] for i in axis_order)
+        key_reinit += tuple(self.key[i] for i in self.axis_order if i not in axis_order_reinit)
+        axis_order_reinit += tuple(i for i in self.axis_order if i not in axis_order_reinit)
 
-        return DatasetView(self.dataset, key_reinit, axis_order_reinit)
+        return DatasetView(self.dataset, (key_reinit, self._int_index), axis_order_reinit)
 
     def read_direct(self, dest, source_sel=None, dest_sel=None):
         """ Using dataset.read_direct, reads data into an existing array
@@ -236,11 +254,18 @@ class DatasetView(h5py.Dataset):
           numpy array
         """
         if source_sel is None:
-            new_key = self.key
+            new_key, new_int_index, new_axis_order = self.key, self._int_index, self.axis_order
         else:
             key_reinit = self._slice_composition(source_sel)
-            _, new_key = self._slice_shape(key_reinit)
-        reversed_axis_order = sorted(range(len(self.axis_order)), key=lambda i: self.axis_order[i])
+            _, new_key, new_int_index, new_axis_order = self._slice_shape(key_reinit)
+        axis_order_slice_iter = new_axis_order
+        for ind in new_int_index:
+            new_axis_order = new_axis_order[:ind[0]] + (ind[2],) + new_axis_order[ind[0]:]
+            new_key = new_key[:ind[0]] + (ind[1],) + new_key[ind[0]:]
+        for ind in reversed(new_int_index):
+            axis_order_slice_iter = tuple(i if i<ind[2] else i-1 for i in axis_order_slice_iter)
+
+        reversed_axis_order = sorted(range(len(new_axis_order)), key=lambda i: new_axis_order[i])
         reversed_slice_key = tuple(new_key[i] for i in reversed_axis_order if i < len(new_key))
         #convert reversed_slice_key to numpy.s_[<args>] format, expected by dataset.read_direct
         if len(reversed_slice_key) == 1:
@@ -258,7 +283,7 @@ class DatasetView(h5py.Dataset):
                 reversed_slice_key = reversed_slice_key[0]
 
         self.dataset.read_direct(reversed_dest, source_sel=reversed_slice_key, dest_sel=reversed_dest_sel)
-        np.copyto(dest, reversed_dest.transpose(self.axis_order))
+        np.copyto(dest, reversed_dest.transpose(axis_order_slice_iter))
 
     def _ellipsis_slices(self, new_slice):
         """ Change Ellipsis dimensions to slices
@@ -273,7 +298,7 @@ class DatasetView(h5py.Dataset):
             if ellipsis_index == len(new_slice)-1:
                 new_slice = new_slice[:-1]
             else:
-                num_ellipsis_dims = len(self.dataset.shape) - (len(new_slice) - 1)
+                num_ellipsis_dims = len(self.shape) - (len(new_slice) - 1)
                 new_slice = new_slice[:ellipsis_index] + np.index_exp[:]*num_ellipsis_dims + new_slice[ellipsis_index+1:]
         elif ellipsis_count > 0:
             raise IndexError("Only a single Ellipsis is allowed")
